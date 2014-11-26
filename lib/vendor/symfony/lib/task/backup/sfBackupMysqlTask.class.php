1f2fff58f08a53be157c3a4c55d94dd148a78623
DH61BE-vbox <dh61be-vbox@localhost>
2014-11-26 12:08:33 -0500

    Fusion symfony 1.4.20 con bootstrap2.3.2
     - Completado la compresion de respaldos *.sql de MySQL con 7zip.
     - Eliminacion del archivo *.sql después de la compresion terminada
     - Testeado la ejecucion de las tareas en Windows 8.1
     - Falta probarlo en Linux

--- lib/vendor/symfony/lib/task/backup/sfBackupMysqlTask.class.php
+++ lib/vendor/symfony/lib/task/backup/sfBackupMysqlTask.class.php
@@ -40,17 +40,17 @@ class sfBackupMysqlTask extends sfGeneratorBaseTask {
         $this->briefDescription    = '>> Genera respaldo de base de datos';
         $this->detailedDescription = <<<EOF
 La tarea [backup:mysql|INFO] crea un respaldo de base de datos con
-extension *.sql a la vez que se le va a comprimir para mejor
+extension *.sql a la vez que comprimime el mismo para mejor
 transporte:
 
   [./symfony backup:mysql nombre_archivo|INFO]
 
 Adicionalmente la instruccion sabe que extension crear para el archivo
-comprimido (nombre_archivo.zip), ademas de incluir adicional a nombre
-de archivo la fecha y hora actual del sistema donde se encuentre 
-instalado.
+comprimido (nombre_archivo.zip), ademas de incluir dentro del archivo 
+comprimido el *.sql del respaldo con fecha y hora del sistema donde 
+se encuentre alojado.
 
-  backup_2014-11-21_172119.zip
+  backup.zip
 
 El directorio por defecto para la ubicacion del respaldo de base de datos 
 es la carpeta docs.
@@ -62,6 +62,7 @@ EOF;
      */
     protected function execute($arguments = array(), $options = array()) {
         $contenedor = array();
+        $filename = $arguments['name']."_".date('Y-m-d')."_".date('His').".sql";
         if ($file = file(sfConfig::get('sf_config_dir').'/databases.yml')) {
             foreach ($file as $k => $v):
                 if (false !== strpos($v, $this->findDns)):
@@ -81,74 +82,27 @@ EOF;
                  . "-h ".$contenedor['hst']." "
                  . "-u ".$contenedor['usr']." "
                  . "-p".$contenedor['pwd']." "
-//                 . "--opt "
-//                 . "--extended-insert=FALSE "
                  . "--skip-extended-insert "
                  . "--single-transaction "
                  . "--quick "
                  . $contenedor['dbn']." > "
                  . sfConfig::get('sf_docs_dir')."/"
-                 . $arguments['name']."_".date('Y-m-d')."_".date('His').".sql";
+                 . $filename;
         exec($command);
-        
+
         $file_compress = "7z "
                        . "a "
                        . "-t7z "
                        . sfConfig::get('sf_docs_dir')."/backup.7z "
                        . sfConfig::get('sf_docs_dir')."/"
-                       . $arguments['name']."_".date('Y-m-d')."_".date('His').".sql "
+                       . $filename
 //                       . "-m0=BCJ2 -m1=LZMA2:d=1024m -aoa";
 //                       . "-mx9 -aoa";
-                       . "-mx9";
-        
-        exec($file_compress);
-        $this->logSection('backup:mysql', sprintf('"%s" :: creado satisfactoriamente...', $contenedor['dbn']));
-        
-//        $app    = $arguments['application'];
-//        $module = $arguments['module'];
+                       . " -mx9";
 
-//        // Se valida el nombre del modulo
-//        if (!preg_match('/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', $module)) {
-//            throw new sfCommandException(sprintf('El nombre del modulo "%s" es invalido.', $module));
-//        }
-//
-//        $moduleDir = sfConfig::get('sf_app_module_dir').'/'.$module;
-//
-//        if (is_dir($moduleDir)) {
-//            throw new sfCommandException(sprintf('El modulo "%s" ya existe en la aplicacion "%s".', $moduleDir, $app));
-//        }
-//
-//        $properties = parse_ini_file(sfConfig::get('sf_config_dir').'/properties.ini', true);
-//
-//        $constants = array(
-//            'PROJECT_NAME' => isset($properties['symfony']['name']) ? $properties['symfony']['name'] : 'symfony',
-//            'APP_NAME'     => $app,
-//            'MODULE_NAME'  => $module,
-//            'AUTHOR_NAME'  => isset($properties['symfony']['author']) ? $properties['symfony']['author'] : 'Aqui tu nombre',
-//            'FECHA_y_HORA' => $this->getDateAndTimeInEs(date('Y-m-d H:i:s')),
-//        );
-//
-//        if (is_readable(sfConfig::get('sf_data_dir').'/skeleton/module')) {
-//            $skeletonDir = sfConfig::get('sf_data_dir').'/skeleton/module';
-//        } else {
-//            $skeletonDir = dirname(__FILE__).'/skeleton/module';
-//        }
-//
-//        // crea una estructura basica para la aplicacion
-//        $finder = sfFinder::type('any')->discard('.sf');
-//        $this->getFilesystem()->mirror($skeletonDir.'/module', $moduleDir, $finder);
-//
-//        // crea un test basico
-//        $this->getFilesystem()->copy($skeletonDir.'/test/actionsTest.php', sfConfig::get('sf_test_dir').'/functional/'.$app.'/'.$module.'ActionsTest.php');
-//
-//        // personaliza el archivo test
-//        $this->getFilesystem()->replaceTokens(
-//            sfConfig::get('sf_test_dir').'/functional/'.$app.DIRECTORY_SEPARATOR.$module.'ActionsTest.php', '##', '##', $constants
-//        );
-//
-//        // customize php and yml files
-//        $finder = sfFinder::type('file')->name('*.php', '*.yml');
-//        $this->getFilesystem()->replaceTokens($finder->in($moduleDir), '##', '##', $constants);
+        exec($file_compress);        
+        $this->logSection('backup:mysql', sprintf('"%s" :: creado satisfactoriamente...', $contenedor['dbn']));
+        unlink(sfConfig::get('sf_docs_dir')."/".$filename);
     }
 
 }
