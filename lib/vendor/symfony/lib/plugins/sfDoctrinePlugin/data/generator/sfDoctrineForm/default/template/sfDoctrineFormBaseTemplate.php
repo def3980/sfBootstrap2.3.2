8d07b772f4f265f15db71f364c48e7096505a0d6
DH61BE-vbox <dh61be-vbox@localhost>
2014-12-05 12:51:01 -0500

    Fusion symfony 1.4.20 con bootstrap2.3.2
     - Reubicando declaraciones ORM en los filtros

--- lib/vendor/symfony/lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineForm/default/template/sfDoctrineFormBaseTemplate.php
+++ lib/vendor/symfony/lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineForm/default/template/sfDoctrineFormBaseTemplate.php
@@ -1,16 +1,16 @@
 [?php
 
 /**
+ * Fecha creacion : "<?=$this->_fechaYHora?>"
+ */
+
+/**
  * Clase base de formulario del Proyecto.
  *
  * @package    ##PROJECT_NAME##
  * @subpackage form
  * @author     ##AUTHOR_NAME##
  * @version    SVN: $Id: sfDoctrineFormBaseTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
- */
- 
-/**
- * Fecha creacion : "<?=$this->_fechaYHora?>"
  */
 abstract class BaseFormDoctrine extends sfFormDoctrine {
 
--- lib/vendor/symfony/lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineFormFilter/default/template/sfDoctrineFormFilterBaseTemplate.php
+++ lib/vendor/symfony/lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineFormFilter/default/template/sfDoctrineFormFilterBaseTemplate.php
@@ -1,16 +1,16 @@
 [?php
 
 /**
+ * Fecha creacion : "<?=$this->_fechaYHora?>"
+ */
+
+/**
  * Clase base de formulario para el filtrado en el Proyecto.
  *
  * @package    ##PROJECT_NAME##
  * @subpackage filter
  * @author     ##AUTHOR_NAME##
  * @version    SVN: $Id: sfDoctrineFormFilterBaseTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
- */
-
-/**
- * Fecha creacion : "<?=$this->_fechaYHora?>"
  */
 abstract class BaseFormFilterDoctrine extends sfFormFilterDoctrine {
 
