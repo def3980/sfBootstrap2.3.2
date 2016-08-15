37401dce6e0516676c26f19405bbbacdc04c9905
def_3980 <def.3980@gmail.com>
2016-08-14 21:25:23 -0500

    Oswaldo  - Mejorado la estructura de espacios para mejor entendimiento    vendor/symfony/lib/autoload

--- lib/vendor/symfony/lib/autoload/sfAutoloadAgain.class.php
+++ lib/vendor/symfony/lib/autoload/sfAutoloadAgain.class.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * + ------------------------------------------------------------------- +
+ * Por Oswaldo Rojas
+ * Añadiendo nuevas formas a lo ya optimizado.
+ * Domingo, 14 Agosto 2016 20:19:38
+ * + ------------------------------------------------------------------- +
+ */
+
 /*
  * This file is part of the symfony package.
  * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
@@ -16,117 +24,101 @@
  * @author     Kris Wallsmith <kris.wallsmith@symfony-project.com>
  * @version    SVN: $Id: sfAutoloadAgain.class.php 33561 2012-10-18 11:42:28Z fabien $
  */
-class sfAutoloadAgain
-{
-  static protected
-    $instance = null;
+class sfAutoloadAgain {
 
-  protected
-    $registered = false,
-    $reloaded   = false;
+    static protected
+        $instance = null;
 
-  /**
-   * Returns the singleton autoloader.
-   * 
-   * @return sfAutoloadAgain
-   */
-  static public function getInstance()
-  {
-    if (null === self::$instance)
-    {
-      self::$instance = new self();
-    }
+    protected
+        $registered = false,
+        $reloaded   = false;
 
-    return self::$instance;
-  }
-
-  /**
-   * Constructor.
-   */
-  protected function __construct()
-  {
-  }
-
-  /**
-   * Reloads the autoloader.
-   * 
-   * @param  string $class
-   * 
-   * @return boolean
-   */
-  public function autoload($class)
-  {
-    // only reload once
-    if ($this->reloaded)
-    {
-      return false;
-    }
-
-    $autoloads = spl_autoload_functions();
-
-    // as of PHP 5.2.11, spl_autoload_functions() returns the object as the first element of the array instead of the class name
-    if (version_compare(PHP_VERSION, '5.2.11', '>='))
-    {
-      foreach ($autoloads as $position => $autoload)
-      {
-        if (is_array($autoload) && $this === $autoload[0])
-        {
-          break;
+    /**
+     * Returns the singleton autoloader.
+     * 
+     * @return sfAutoloadAgain
+     */
+    static public function getInstance() {
+        if (null === self::$instance) {
+            self::$instance = new self();
         }
-      }
-    }
-    else
-    {
-      $position  = array_search(array(__CLASS__, 'autoload'), $autoloads, true);
+
+        return self::$instance;
     }
 
-    if (isset($autoloads[$position + 1]))
-    {
-      $this->unregister();
-      $this->register();
+    /**
+     * Constructor.
+     */
+    protected function __construct() {}
 
-      // since we're rearranged things, call the chain again
-      spl_autoload_call($class);
+    /**
+     * Reloads the autoloader.
+     * 
+     * @param  string $class
+     * 
+     * @return boolean
+     */
+    public function autoload($class) {
+        // only reload once
+        if ($this->reloaded) {
+            return false;
+        }
 
-      return class_exists($class, false) || interface_exists($class, false);
+        $autoloads = spl_autoload_functions();
+
+        // as of PHP 5.2.11, spl_autoload_functions() returns the object as the first element of the array instead of the class name
+        if (version_compare(PHP_VERSION, '5.2.11', '>=')) {
+            foreach ($autoloads as $position => $autoload) {
+                if (is_array($autoload) && $this === $autoload[0]) {
+                    break;
+                }
+            }
+        } else {
+            $position  = array_search(array(__CLASS__, 'autoload'), $autoloads, true);
+        }
+
+        if (isset($autoloads[$position + 1])) {
+            $this->unregister();
+            $this->register();
+
+            // since we're rearranged things, call the chain again
+            spl_autoload_call($class);
+
+            return class_exists($class, false) || interface_exists($class, false);
+        }
+
+        $autoload = sfAutoload::getInstance();
+        $autoload->reloadClasses(true);
+
+        $this->reloaded = true;
+
+        return $autoload->autoload($class);
     }
 
-    $autoload = sfAutoload::getInstance();
-    $autoload->reloadClasses(true);
-
-    $this->reloaded = true;
-
-    return $autoload->autoload($class);
-  }
-
-  /**
-   * Returns true if the autoloader is registered.
-   * 
-   * @return boolean
-   */
-  public function isRegistered()
-  {
-    return $this->registered;
-  }
-
-  /**
-   * Registers the autoloader function.
-   */
-  public function register()
-  {
-    if (!$this->isRegistered())
-    {
-      spl_autoload_register(array($this, 'autoload'));
-      $this->registered = true;
+    /**
+     * Returns true if the autoloader is registered.
+     * 
+     * @return boolean
+     */
+    public function isRegistered() {
+        return $this->registered;
     }
-  }
 
-  /**
-   * Unregisters the autoloader function.
-   */
-  public function unregister()
-  {
-    spl_autoload_unregister(array($this, 'autoload'));
-    $this->registered = false;
-  }
+    /**
+     * Registers the autoloader function.
+     */
+    public function register() {
+        if (!$this->isRegistered()) {
+            spl_autoload_register(array($this, 'autoload'));
+            $this->registered = true;
+        }
+    }
+
+    /**
+     * Unregisters the autoloader function.
+     */
+    public function unregister() {
+        spl_autoload_unregister(array($this, 'autoload'));
+        $this->registered = false;
+    }
 }
